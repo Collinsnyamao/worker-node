feeb037e281325eefd0231e8f125a26b52cfc374
Collins nyamao <31241851+Collinsnyamao@users.noreply.github.com>
2025-05-09 13:16:59 +0300

    w.n.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const config = require('./config');
+const config = require('./config/config');
 const { connect, disconnect } = require('./services/websocket');
 const { registerCommandHandler } = require('./services/commands');
 
